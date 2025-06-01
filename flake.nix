515b13e916710c83d5c3d931e79bf5da26d91117
msladecek <martin.sladecek@gmail.com>
2025-06-01 23:06:52 +0200

    add formatter and environment config

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/24.11";
+    flake-utils.url = "github:numtide/flake-utils";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+  };
+  outputs =
+    inputs@{
+      self,
+      nixpkgs,
+      flake-utils,
+      treefmt-nix,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        treefmt-config = {
+          projectRootFile = "flake.nix";
+          programs.cljfmt.enable = true;
+          programs.cljfmt.includes = [ "simple-test" ];
+          programs.black.enable = true;
+          programs.nixfmt.enable = true;
+        };
+      in
+      {
+        formatter = treefmt-nix.lib.mkWrapper pkgs treefmt-config;
+        devShells.default = pkgs.mkShell {
+          name = "simple-test";
+          buildInputs = with pkgs; [
+            python
+            babashka
+          ];
+        };
+      }
+    );
+}
